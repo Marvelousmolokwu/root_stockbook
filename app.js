d4878e9383f2bfb7fa092afafb4ecf42b149b901
Marvelousmolokwu <qmolokwu@gmail.com>
2023-04-04 16:16:38 +0100

    i added the dashborad

--- app.js
+++
@@ -1,9 +0,0 @@
-const toggleBtn = document.querySelector(".sidebar-toggle");
-const closeBtn = document.querySelector(".close-btn");
-const dashboard = document.querySelector(".dashBoard");
-toggleBtn.addEventListener("click", function () {
-  dashboard.classList.toggle("show-sidebar");
-});
-closeBtn.addEventListener("click", function () {
-  dashboard.classList.remove("show-sidebar");
-});
